95eccc6ae8de72f1eded50d0daa7a9304b62076f
Kevin Hu <khu@liveperson.com>
2020-08-26 17:22:44 -0400

    added basic auth and changed some google apis logic

--- server.js
+++ server.js
@@ -8,32 +8,82 @@ let credentials = require("./credentials");
 const sheets = google.sheets("v4");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send("<h1>HELLO JEREMY</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>HELLO JEREMY</h1>");
+});
 
-app.get('/getDataValues', async (req, res) => {
-    var doc = new GoogleSpreadsheet();
+app.post("/append-order-number", async (req, res) => {
+  let { orderNumber } = req.body;
+  let { username, password, ss_id} = process.env;
+  console.log(req.headers.authorization);
+  //basicauth = <base64 encrypted version of `Basic <username>:<password>`>
+  let basicauth = Buffer.from(req.headers.authorization.slice(6), 'base64').toString('binary');
+  // basicauth = "jeremy:andrews"
+  basicauth = basicauth.split(":");
+  console.log(basicauth);
+  if(username != basicauth[0] || password != basicauth[1]) {
+    console.log(username, basicauth[0], "    ", password, basicauth[1])
+    res.status(401).send({message: "Unauthorized!"})
+  }
+
+  if (!orderNumber) {
+    orderNumber = "XSFJQ2";
+    // res.status(404).send({message: "No order number was sent."})
+  }
+  var doc = new GoogleSpreadsheet();
   await doc.useServiceAccountAuth(credentials);
   const auth = new google.auth.OAuth2();
   auth.setCredentials(doc.jwtClient.credentials);
   console.log("AUTH1: ", auth);
-  sheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId: process.env.ss_id,
-      range: "A1:G"
-  },
-  (err, response) => {
-      if(err) {
-          console.log(err)
-          return res.status(400).json(err.data);
-      }
-      res.send(response);
-  })
-})
+  let r = await sheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId: ss_id,
+    range: "A1:B"
+  });
+
+  console.log(r);
+  r = r.data.values;
+  let edited;
+  for (let i = 0; i < r.length; i++) {
+    let n = r[i][0];
+    let x = r[i][1];
+    if (n == orderNumber) {
+      r[i][1] += r[i][0];
+      edited = i;
+      break;
+    } else if (i == r.length - 1) {
+      res.status(404).send({ message: "Order number does not exist." });
+    }
+  }
+
+  let body = {
+    values: r
+  };
+  res.send(r);
+  // sheets.spreadsheets.values.update(
+  //   {
+  //     auth,
+  //     spreadsheetId: ss_id,
+  //     range: "A1:B",
+  //     valueInputOption: "USER_ENTERED",
+  //     resource: body
+  //   },
+  //   (err, response) => {
+  //     if(err) {
+  //       console.log(err)
+  //       res.status(500);
+  //     }
+  //     var result = response.result;
+  //     console.log(result);
+  //     console.log(`${result.updatedCells} cells updated.`);
+  //     res.send({ updated: result.updatedCells });
+  //   }
+  // );
+
+});
 
 app.listen(port, () => {
-    console.log(`listening to port ${port}`)
-})
+  console.log(`listening to port ${port}`);
+});
